79e693db05b13a57405c7d745cb4a6179c1a9c80
SoulfulRain <RainSoul1120@gmail.com>
2024-05-04 11:19:39 +0800

    Spring启示录

---
+++ spring6-001-revelation/src/main/java/com/rainsoul/spring6/client/Test.java
@@ -0,0 +1,11 @@
+package com.rainsoul.spring6.client;
+
+
+import com.rainsoul.spring6.web.UserAction;
+
+public class Test {
+    public static void main(String[] args) {
+        UserAction userAction = new UserAction();
+        userAction.deleteRequest();
+    }
+}
