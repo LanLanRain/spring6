918c7a675c5c5dfcc6d998e9a5c14698b3367bba
SoulfulRain <RainSoul1120@gmail.com>
2024-05-04 12:52:15 +0800

    set注入和构造方法注入

--- spring6-002-first/src/main/java/com/rainsoul/spring6/bean/User.java
+++ spring6-002-first/src/main/java/com/rainsoul/spring6/bean/User.java
@@ -4,7 +4,7 @@ public class User {
     // Spring是怎么实例化对象的？
     // 默认情况下Spring会通过反射机制，调用类的无参数构造方法来实例化对象。
     // 实现原理如下：
-    // Class clazz = Class.forName("com.powernode.spring6.bean.User");
+    // Class clazz = Class.forName("com.rainsoul.spring6.bean.User");
     // Object obj = clazz.newInstance();
     public User() {
         System.out.println("User的无参数构造方法执行。");
---
+++ spring6-003-dependency-injection/src/test/java/com/rainsoul/spring6/test/SpringDITest.java
@@ -0,0 +1,29 @@
+package com.rainsoul.spring6.test;
+
+import com.rainsoul.spring6.service.CustomerService;
+import com.rainsoul.spring6.service.UserService;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringDITest {
+    @Test
+    public void testSetDI(){
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        UserService userService = applicationContext.getBean("userServiceBean", UserService.class);
+        userService.saveUser();
+    }
+
+    @Test
+    public void testConstructorDI(){
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+        CustomerService customerService = applicationContext.getBean("csBean", CustomerService.class);
+        customerService.save();
+
+        CustomerService csBean2 = applicationContext.getBean("csBean2", CustomerService.class);
+        csBean2.save();
+
+        CustomerService csBean3 = applicationContext.getBean("csBean3", CustomerService.class);
+        csBean3.save();
+    }
+}
